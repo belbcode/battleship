0c7438b31adc6d54bf724122cfbe24123c425c1e
Sergey Gilyazov <s.gilyazov@gmail.com>
2023-01-29 20:11:07 +0200

    difault selected ship removed

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -9,16 +9,13 @@ export const GameContext = createContext({ handleCellClick: () => {} });
 
 const Game = () => {
     const initField = new GameField(10, 10, "empty");
-    for (let i = 1; i < 4; i++) {
-        initField.changeState([i, 1], "ship");
-    }
 
     const [field, setField] = useState(initField);
-    const aaa = new GameField(1, 2, "ship").field;
-    const [selectedShip, setSelectedShip] = useState(aaa);
+
+    const [selectedShip, setSelectedShip] = useState();
 
     const handleCellClick = (coords) => {
-        console.log("setting up a ship");
+        if (!selectedShip) return;
         setField((prev) => {
             const newGameField = cloneDeep(prev);
             newGameField.addShip(selectedShip, coords);

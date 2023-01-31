9c21d622ad9b2c1c0c922e8d61986b894c151a30
Sergey Gilyazov <s.gilyazov@gmail.com>
2023-01-31 17:14:33 +0200

    hotfix

--- src/lib/initShips.js
+++ src/lib/initShips.js
@@ -2,15 +2,15 @@ import GameField from "./gameField";
 
 const initShips = [
     { id: "shipP1", gameField: new GameField(1, 5, "ship") },
-    // { id: "shipP2", gameField: new GameField(1, 4, "ship") },
-    // { id: "shipP3", gameField: new GameField(1, 4, "ship") },
-    // { id: "shipP4", gameField: new GameField(1, 3, "ship") },
-    // { id: "shipP5", gameField: new GameField(1, 3, "ship") },
-    // { id: "shipP6", gameField: new GameField(1, 3, "ship") },
-    // { id: "shipP7", gameField: new GameField(1, 2, "ship") },
-    // { id: "shipP8", gameField: new GameField(1, 2, "ship") },
-    // { id: "shipP9", gameField: new GameField(1, 2, "ship") },
-    // { id: "shipP10", gameField: new GameField(1, 2, "ship") },
+    { id: "shipP2", gameField: new GameField(1, 4, "ship") },
+    { id: "shipP3", gameField: new GameField(1, 4, "ship") },
+    { id: "shipP4", gameField: new GameField(1, 3, "ship") },
+    { id: "shipP5", gameField: new GameField(1, 3, "ship") },
+    { id: "shipP6", gameField: new GameField(1, 3, "ship") },
+    { id: "shipP7", gameField: new GameField(1, 2, "ship") },
+    { id: "shipP8", gameField: new GameField(1, 2, "ship") },
+    { id: "shipP9", gameField: new GameField(1, 2, "ship") },
+    { id: "shipP10", gameField: new GameField(1, 2, "ship") },
 ];
 
 export default initShips;
